b83ef3737e34ea43aecdcd9a8d2334f045decc1a
Andrew <github.spmh8tr@spamgourmet.com>
2024-09-27 00:48:10 -0400

    V0.1.1 Added server side variables to hold door condition Added message persistence Added other things

--- server.js
+++ server.js
@@ -9,31 +9,56 @@ const io = new Server(server);
 // Serve static files
 app.use(express.static('public'));
 
+let state = {
+  isGroupReady: false,
+  isEmergencyActive: false,
+  chatHistory: []
+};
+
+
+
 // WebSocket connection handling
 io.on('connection', (socket) => {
   console.log('A user connected');
 
+  socket.emit('state_update', state);
+
+  socket.on('request_status', () => {
+    socket.broadcast.emit('request_status'); // Forward the request to other clients (the door)
+  });
+
   socket.on('dispatch_ready', () => {
-    io.emit('group_ready');  // Notify door side that group is ready
+    state.isGroupReady = true;
+    io.emit('group_ready');
+    io.emit('state_update', { isGroupReady: true }); // Broadcast state change
   });
 
   socket.on('group_sent', () => {
-    io.emit('reset_screens');  // Reset both sides
+      state.isGroupReady = false;
+      io.emit('group_sent_confirmation');
+      io.emit('reset_screens');
+      io.emit('state_update', { isGroupReady: false });
   });
 
   // Chat function
   socket.on('chat_message', (msg) => {
-    io.emit('receive_message', msg);  // Send message to both sides
+    state.chatHistory.push(msg);
+    io.emit('receive_message', msg);
+    io.emit('state_update', { chatHistory: state.chatHistory }); // Broadcast state change
   });
 
   // Emergency alert event
   socket.on('emergency_alert', () => {
-    io.emit('emergency_alert');  // Alert dispatch about emergency
+      state.isEmergencyActive = true;
+      io.emit('emergency_alert_active');
+      io.emit('state_update', { isEmergencyActive: true });
   });
 
   // Clear emergency event
   socket.on('clear_emergency', () => {
-    io.emit('clear_emergency');  // Clear emergency on both sides
+      state.isEmergencyActive = false;
+      io.emit('emergency_alert_cleared');
+      io.emit('state_update', { isEmergencyActive: false });
   });
 
   socket.on('disconnect', () => {
